6456dc0c511ab004bef26aeec88e21a81915e166
Ferry Alexandre <alexandre.ferry2024@campus-eni.fr>
2024-09-10 11:32:58 +0200

    Utilisateur_V01.1(avant template(connexion))

--- encheres/src/main/java/fr/eni/encheres/repository/UtilisateurDAOImpl.java
+++ encheres/src/main/java/fr/eni/encheres/repository/UtilisateurDAOImpl.java
@@ -5,37 +5,63 @@ package fr.eni.encheres.repository;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Timestamp;
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
-import fr.eni.encheres.bo.ArticleVendu;
-import fr.eni.encheres.bo.Enchere;
 import fr.eni.encheres.bo.Utilisateur;
 
 /**
  * Classe en charge de
  * 
  * @projet : encheres - V1.0
- * @author : aferry2024
+ * @author : kim, ophélie, alex
  * @since: 9 sept. 2024 - 13:34:57
  */
 @Repository
 public class UtilisateurDAOImpl implements UtilisateurDAO {
-	//Jointure Utilisateurs+Encheres+Articles_Vendus
-	private  final String FIND_ALL ="SELECT no_utilisateur,pseudonom,prenom,email,telephone,rue,code_postal,ville,mot_de_passe,credit,administrateurFROM UTILISATEURS;";
-			
+	// Jointure Utilisateurs+Encheres+Articles_Vendus
+	private final String INSERT = "INSERT INTO FILM(pseudo,nom,prenom,email,telephone,rue,code_postal,ville,mot_de_passe,credit,administrateur) "
+			+ " VALUES (:pseudo,:nom,:prenom,:email,:telephone,:rue,:code_postal,:ville,:mot_de_passe,:credit,:administrateur)";
+	private final String FIND_ALL = "SELECT no_utilisateur,pseudo,nom,prenom,email,telephone,rue,code_postal,ville,mot_de_passe,credit,administrateur"
+			+ "FROM UTILISATEURS;";
+	private final String FIND_BY_PSEUDO = "SELECT no_utilisateur,pseudo,nom,prenom,email,telephone,rue,code_postal,ville,mot_de_passe,credit,administrateur"
+			+ "FROM UTILISATEURS " + "WHERE PSEUDO=?;";
+	private final String FIND_BY_EMAIL = "SELECT no_utilisateur,pseudo,nom,prenom,email,telephone,rue,code_postal,ville,mot_de_passe,credit,administrateur"
+			+ "FROM UTILISATEURS WHERE EMAIL=?;";
 	@Autowired
-	private JdbcTemplate jdbcTemplate;
+	private NamedParameterJdbcTemplate jdbcTemplate;
 
 	@Override
 	public void create(Utilisateur utilisateur) {
-		// TODO Auto-generated method stub
+		KeyHolder keyHolder = new GeneratedKeyHolder();
 		
+		MapSqlParameterSource namedParameters = new MapSqlParameterSource();
+		namedParameters.addValue("pseudo", utilisateur.getPseudo());
+		namedParameters.addValue("nom", utilisateur.getNom());
+		namedParameters.addValue("prenom", utilisateur.getPrenom());
+		namedParameters.addValue("email", utilisateur.getEmail());
+		namedParameters.addValue("telephone", utilisateur.getTelephone());
+		namedParameters.addValue("rue", utilisateur.getRue());
+		namedParameters.addValue("code_postal", utilisateur.getCodePostal());
+		namedParameters.addValue("mot_de_passe", utilisateur.getMotDePasse());
+		namedParameters.addValue("credit", utilisateur.getPseudo());
+		namedParameters.addValue("administrateur", utilisateur.getAdministrateur());
+		
+		jdbcTemplate.update(INSERT, namedParameters, keyHolder);
+		
+		if (keyHolder != null && keyHolder.getKey() != null) {
+			utilisateur.setNoUtilisateur(keyHolder.getKey().intValue());
+		}
+		
+
 	}
 
 	@Override
@@ -80,18 +106,7 @@ public class UtilisateurDAOImpl implements UtilisateurDAO {
 			u.setVille(rs.getString("ville"));
 			u.setMotDePasse(rs.getString("mot_de_passe"));
 			u.setCredit(rs.getInt("credit"));
-			u.setAdministrateur(rs.getByte("administrateur"));
-			
-			//Association Encheres
-			Enchere e = new Enchere();
-			//Pour mapper le champ dateEnchere
-			Timestamp timestamp =rs.getTimestamp("date_enchere");
-			if (timestamp != null) {
-				e.setDateEnchere(timestamp.toLocalDateTime());
-			}
-			e.setMontant_enchere(rs.getInt("montant_enchere"));
-			u.setEnchere(e);
-
+			u.setAdministrateur(rs.getBoolean("administrateur"));
 
 			return u;
 		}
